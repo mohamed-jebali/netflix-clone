ff51d05b6ab6086c541a144721454727a7577f27
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-10-25 20:36:20 +0200

    base structure to dasboard layout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,25 @@
 @extends('layouts.app')
 
-@section('main-content')
-<h1 class="text-primary">
-    this is the dashboard
-</h1>
+@section('content')
+
+
+<div class="dashboard-wrapper">
+    <div class="container-fluid">
+        <div class="row">
+
+        <!-- SIDEBAR -->
+            <div class="col-2 sidebar bg-primary">
+                sidebar
+            </div>
+
+            <!-- DASHBOARD-CONTENT -->
+            <div class="col-10 bg-danger dashboard-content">
+                <h1 class="text-primary text-center">
+                    this is your'e the dashboard hello {{ auth()->user()->name }}  {{ auth()->user()->surname }}
+                </h1>
+            </div>
+        <div>
+    </div>
+</div>
+      
 @endsection
